f95fc366885a2bb6f362658742fdf14d8ff7e665
Stone.F.Yang (netw it.twtp01.Newegg) 61589 <sy43@buyabs.corp>
2018-06-15 15:53:41 +0800

    type hints

--- typeHintsEx.py
+++ typeHintsEx.py
@@ -1,3 +1,5 @@
+from typing import Dict, Tuple, List, Callable, NamedTuple
+
 #ex1
 def two_sum(a: int, b: int) -> int:
     return a + b
@@ -6,7 +8,7 @@ print(two_sum.__annotations__)
 
 print(two_sum(2, 1)) # result: 3
 
-# class ex
+#Class ex
 class Point():  
     def __init__(self, x, y): 
         self.x = x 
@@ -18,12 +20,22 @@ def output_point(point:Point):
 output_point(Point(1,3))
 
 # NamedTuple ex
-import typing
-Point2 = typing.NamedTuple('Point', x=int, y=int)
+Point2 = NamedTuple('Point2', x=int, y=int)
 
 def output_point2(point:Point2):
     print(point.x)
 
-output_point2(Point(2,4))       
+output_point2(Point2(2,4))       
 
-  
+
+#ex Dict, Tuple, List
+ConnectionOptions = Dict[str, str]
+Address = Tuple[str, str]
+Server = Tuple[Address, ConnectionOptions]
+
+def broadcast_message(message: str, servers: List[Server]) -> None:
+    for server in servers:
+        print(server)    
+    print(message)
+
+broadcast_message("hi!",[("a","b"),("c","d")])
