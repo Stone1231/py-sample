81d83f1318b4864a5938c6b48eb7fffe47737643
stone <stone.yang@nt-tech.tw>
2020-12-04 12:27:39 +0800

    launch.json update

--- dsa/tuple_ex.py
+++ dsa/tuple_ex.py
@@ -5,15 +5,15 @@ Making a tuple
 '''
 dimensions = (1920, 1080)
 for dimension in dimensions:
-    print(dimension)
+      print(dimension)
 
 for i, dimension in enumerate(dimensions):
-    print(i, dimension)    
+    print(i, dimension)
     #dimensions[i] = dimensions[i] + 100000  不能更新異動
 
-players = [] 
-if players: 
-    for player in players: 
-        print("Player: " + player.title()) 
-else: 
+players = []
+if players:
+    for player in players:
+        print("Player: " + player.title())
+else:
     print("We have no players yet!")
